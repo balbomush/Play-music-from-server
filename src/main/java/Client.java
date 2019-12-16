3fd8f254c438b4f73232160472c6be416566ffbe
balbomush <32074067+balbomush@users.noreply.github.com>
2019-12-16 20:21:50 +0300

    delete old comments

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,65 +4,43 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
 
 public class Client {
 
-    private static Socket clientSocket; //сокет для общения
-    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
-    // мы узнаем что хочет сказать клиент?
-    private static DataInputStream in; // поток чтения из сокета
-    //private static BufferedWriter out; // поток записи в сокет
+    private static Socket clientSocket;
+    private static BufferedReader reader;
+    private static DataInputStream in;
 
     public static void main(String[] args) throws IOException {
         try{
             clientSocket = new Socket("localhost", 4004);
             reader = new BufferedReader(new InputStreamReader(System.in));
             in = new DataInputStream(clientSocket.getInputStream());
+
             byte byteArray[] = new byte[1024];
-            //ByteArrayInputStream bis = new ByteArrayInputStream(byteArray);
-            //ArrayList<byte[]> arrayList = new ArrayList<>();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            //String bytes = new String();
             int count = 0;
             while ((count=in.read(byteArray)) != -1){
                 outputStream.write(byteArray,0,count);
             }
-            //outputStream.close();
+            System.out.println(outputStream.size());
 
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+            InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
             GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
             ByteArrayOutputStream tmpout = new ByteArrayOutputStream();
-            while ((count=inputStream.read(byteArray)) != -1){
+            count = 0;
+            System.out.println("start decompressed");
+            while ((count=gzipInputStream.read(byteArray)) != -1){
                 tmpout.write(byteArray,0,count);
             }
+            System.out.println("end decompressed");
             inputStream = new ByteArrayInputStream(tmpout.toByteArray());
             System.out.println("start play");
             InputStream potok = new BufferedInputStream(inputStream);
             AudioDevice auDev = new JavaSoundAudioDevice();
             AdvancedPlayer explay = new AdvancedPlayer(potok,auDev);
             explay.play();
-            //System.out.println(bytes);
-            //InputStream stream = new ByteArrayInputStream(bytestring.getBytes(StandardCharsets.UTF_8));
-            //AudioDevice auDev = new JavaSoundAudioDevice();
-            //AdvancedPlayer explay = new AdvancedPlayer(stream,auDev);
-            //explay.play();
-            //out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            //String serverWord = in.readLine(); // ждём, что скажет сервер
-            //System.out.println(serverWord);
-
-            //System.out.println("Вы что-то хотели сказать? Введите это здесь:");
-            // если соединение произошло и потоки успешно созданы - мы можем
-            //  работать дальше и предложить клиенту что то ввести
-            // если нет - вылетит исключение
-            //String word = reader.readLine(); // ждём пока клиент что-нибудь
-            // не напишет в консоль
-           // out.write("word" + "\n"); // отправляем сообщение на сервер
-            //out.flush();
 
 
         } catch (Exception e){
@@ -71,7 +49,6 @@ public class Client {
         finally {
             clientSocket.close();
             in.close();
-            //out.close();
         }
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,32 +5,29 @@ import java.util.zip.GZIPOutputStream;
 
 public class Server {
 
-    private static Socket clientSocket; //сокет для общения
-    private static ServerSocket server; // серверсокет
-  //  private static BufferedReader in; // поток чтения из сокета
-    private static DataOutputStream out; // поток записи в сокет
+    private static Socket clientSocket;
+    private static ServerSocket server;
+    private static DataOutputStream out;
 
     public static void main(String[] args) throws IOException {
         try{
             server = new ServerSocket(4004);
             clientSocket = server.accept();
             System.out.println("Connected");
-            //in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new DataOutputStream(clientSocket.getOutputStream());
             BufferedInputStream file = new BufferedInputStream(new FileInputStream("src/main/resources/example.mp3"));
             ByteArrayOutputStream outtmp = new ByteArrayOutputStream();
             GZIPOutputStream compression = new GZIPOutputStream(outtmp);
-            //ByteArrayInputStream intmp = new ByteArrayInputStream();
             byte[] byteArray = new byte[1024];
             int len;
             while ((len=file.read(byteArray)) != -1){
-                outtmp.write(byteArray,0,len);
+                compression.write(byteArray,0,len);
             }
+            compression.close();
+            System.out.println(outtmp.size());
             ByteArrayInputStream intmp = new ByteArrayInputStream(outtmp.toByteArray());
             while (true){
                 int local = intmp.read(byteArray);
-                //System.out.println(byteArray);
-                //System.out.println(local);
                 if (local != -1) {
                     out.write(byteArray,0,local);
                 }
@@ -40,21 +37,12 @@ public class Server {
             }
             out.flush();
             file.close();
-            //out.close();
-
-
-            //out.flush(); // выталкиваем все из буфера
-            //String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
-            //System.out.println(word);
-            // не долго думая отвечает клиенту
-
 
         } catch (Exception e){
             System.out.println(e);
         }
         finally {
             server.close();
-            //in.close();
             out.close();
         }
     }
